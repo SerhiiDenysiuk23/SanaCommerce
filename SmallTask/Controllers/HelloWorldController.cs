ba396dfebb82f0ad88dc96dfcd8d3a839f5f5817
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2022-04-12 17:35:38 +0300

    First task complete

---
+++ SmallTask/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using SmallTask.Models;
+
+namespace SmallTask.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+                return View("Welcome", user);
+            else
+                return View();
+        }
+
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            return View();
+        }
+    }
+}
---
+++ SmallTask/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmallTask.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "You didn't input name")]
+        public string Name { get; set; }
+    }
+}
