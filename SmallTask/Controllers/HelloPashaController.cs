fa9118bc3f28aa0e7fb67513b044a0efe46dbfec
Сергій Денисюк <kn202_dsm@student.ztu.edu.ua>
2022-04-17 20:08:25 +0300

    HelloPashaController

---
+++ SmallTask/Controllers/HelloPashaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmallTask.Controllers
+{
+    public class HelloPashaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
